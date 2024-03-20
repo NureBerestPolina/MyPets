f2bfaad49b1e4f5f6e9c36380bc0ca3b87328e26
Polina Berest <pamapolya@gmail.com>
2024-03-20 21:03:29 +0200

    Add project files.

---
+++ Models/Owner.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyPets.Models
+{
+    public class Owner
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        [BsonElement("ownerName")]
+        public string OwnerName { get; set; }
+
+        [BsonElement("country")]
+        public string Country { get; set; }
+
+        [BsonElement("gender")]
+        public string Gender { get; set; }
+
+        [BsonElement("registeredAt")]
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/Pet.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyPets.Models
+{
+    public class Pet
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("breed")]
+        public string Breed { get; set; }
+
+        [BsonElement("specie")]
+        public string Specie { get; set; }
+
+        [BsonElement("ownerId")]
+        public Guid OwnerId { get; set; }
+
+        [BsonElement("addedAt")]
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("updatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
