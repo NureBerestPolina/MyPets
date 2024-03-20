1a5abfbe4110ee52347aaee9949d479ca9e65424
Polina Berest <pamapolya@gmail.com>
2024-03-20 21:25:59 +0200

    -

---
+++ Repositories/PetRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyPets.Repositories
+{
+    public class PetRepository
+    {
+    }
+}
