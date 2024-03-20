a21f17a6729d2327156fe48ac577c8f9265f2f49
Polina Berest <pamapolya@gmail.com>
2024-03-20 21:43:07 +0200

    -

---
+++ Extensions/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MyPets.Folder
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetNameFromToken(this HttpContext context)
+        {
+            Claim userClaim = context?.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+            return userClaim?.Value;
+        }
+    }
+}
